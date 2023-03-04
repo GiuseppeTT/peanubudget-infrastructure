38180e1ccbb5c240a21e791662da1e4342d48893
GiuseppeTT <giu.tinti@gmail.com>
2023-03-04 19:05:39 +0000

    Rename azurerm_role_assignment to `this`

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "azurerm_kubernetes_cluster" "this" {
   }
 }
 
-resource "azurerm_role_assignment" "example" {
+resource "azurerm_role_assignment" "this" {
   principal_id                     = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.this.id
